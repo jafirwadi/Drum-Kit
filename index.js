018c46434921dbb05f6dae9f5616693db71c2b37
Md Tanzim Hossain Mridha <tanzimjafirwadi@gmail.com>
2025-07-26 20:10:57 +0600

    Update index.js

--- index.js
+++ index.js
@@ -19,37 +19,37 @@ function KeyboardSound(key) {
     
     switch (key) {
             case "w": 
-                var Crash = new Audio("sounds/crash.mp3");
+                var Crash = new Audio("crash.mp3");
                 Crash.play();
                 break;
             
             case "a": 
-                var KickBass = new Audio("sounds/kick-bass.mp3");
+                var KickBass = new Audio("kick-bass.mp3");
                 KickBass.play();
                 break;
 
             case "s":
-                var Snare = new Audio("sounds/snare.mp3");
+                var Snare = new Audio("snare.mp3");
                 Snare.play();
                 break;
 
             case "d":
-                var tom1 = new Audio("sounds/tom-1.mp3");
+                var tom1 = new Audio("tom-1.mp3");
                 tom1.play();
                 break;
 
             case "j":
-                var tom2 = new Audio("sounds/tom-2.mp3");
+                var tom2 = new Audio("tom-2.mp3");
                 tom2.play();
                 break;
             
             case "k":
-                var tom3 = new Audio("sounds/tom-3.mp3");
+                var tom3 = new Audio("tom-3.mp3");
                 tom3.play();
                 break;
             
             case "l":
-                var tom4 = new Audio("sounds/tom-4.mp3");
+                var tom4 = new Audio("tom-4.mp3");
                 tom4.play();
                 break;
 
